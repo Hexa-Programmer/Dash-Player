857a51e73392c17e09be56b36baed950b955bb3e
Siddharth <siddharthgandotra2010@gmail.com>
2026-07-05 16:17:24 +0530

    Added UI and functionality to import .mp3 files

--- playerscript.js
+++ playerscript.js
@@ -2,8 +2,14 @@
 const playbutton = document.getElementById("play")
 const skipnextbutton = document.getElementById("skipnext")
 const skipbackbutton = document.getElementById("skipback")
-const audioplayer = document.getElementById("audioplayer")
 const playlist = ["Music/letithappen.mp3", "Music/thelessiknowthebetter.mp3", "Music/pushthefeelingon.mp3", "Music/letmetakeyoudancing.mp3"]
+const audioplayer = document.getElementById("audioplayer")
+const songtitle = document.querySelector(".song-title");
+const dropArea = document.querySelector(".drop-area");
+
+
+
+
 let currenttrackindex = 0; //chooses the song in the list of the "playlist" variable
 //logic for play and pause
 let isplaying = false;
@@ -18,6 +24,7 @@ else if(isplaying === true){
     audioplayer.pause();
     playbutton.innerText = "Play";
     isplaying = false;
+}
 }
 
 skipnextbutton.onclick = function(){
@@ -43,4 +50,33 @@ skipbackbutton.onclick = function(){
         playbutton.innerText = "Pause"
     }
 }
-}
+
+dropArea.addEventListener('dragover', function(event){
+    event.preventDefault();
+});
+
+dropArea.addEventListener('drop', function(event){
+    event.preventDefault();
+});
+
+window.addEventListener('dragover', function(event){ //stop the browser from opening the file in a new tab
+    event.preventDefault();
+});
+
+window.addEventListener('drop', function(event){ //make the browser actually catch the file when dropped
+    event.preventDefault();
+    console.log(event.dataTransfer.files); //allow the browser to read the metadata of the file ex: file name, type, size etc.
+
+    const droppedFile = event.dataTransfer.files[0];
+
+    if(droppedFile){
+        const temporaryURL = URL.createObjectURL(droppedFile);
+
+    audioplayer.src = temporaryURL;
+    songtitle.innerText = droppedFile.name;
+
+    audioplayer.play()
+        playbutton.innerText = "Pause";
+        isplaying = true;
+    }
+});
